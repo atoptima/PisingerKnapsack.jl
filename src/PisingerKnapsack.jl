25453397abd2bb41bb39f68bd2da2fdac2c155f6
Guillaume Marques <guillaume.marques@protonmail.com>
2020-05-16 13:06:43 +0200

    include depsfile

--- src/PisingerKnapsack.jl
+++ src/PisingerKnapsack.jl
@@ -2,6 +2,13 @@ __precompile__()
 
 module PisingerKnapsack
 
+const deps_file = joinpath(dirname(@__FILE__), "..", "deps", "deps.jl")
+if isfile(deps_file)
+    include(deps_file)
+else
+    error("PisingerKnapsack not properly installed. Please run import Pkg; Pkg.build(\"PisingerKnapsack\")")
+end
+
 include("Cwrapper.jl")
 include("doubleknp.jl")
 
